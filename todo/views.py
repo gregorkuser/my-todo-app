4162df7da06f21e3b10b1013de23107ae27a234d
gregorkuser <gregor.kuser@gmail.com>
2025-07-25 14:42:53 +0200

    to-do before upgrade

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect('/')
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    Task.objects.get(id=task_id).delete()
+    return redirect('/')
